5adb1bfc0856a8f55a2274e2c28ba7a4c44ec9cb
Egor <egzag11@gmail.com>
2023-11-30 09:40:21 +0300

    git commit -m "lab_2 commit"

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import asyncio
 import logging
 import sys
 
-from haendler import cmd_inline, cmd_random
-from loader import dp, bot
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from config import TOKEN
+from heandler import start, inline, help_bot
 
 
-async def main() -> None:
 
-    dp.message.register(cmd_random)
-    dp.message.register(cmd_inline)
+async def main () -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_routers(
+        start.router_start,
+        inline.router_inline,
+        help_bot.router_help
+
+    )
     await dp.start_polling(bot)
 
 
